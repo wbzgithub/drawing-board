14abbac3fbb293e25d26bad4b8fa00b7ca586a64
飘飞的鼻涕 <874795821@qq.com>
2021-06-30 17:31:37 +0800

    ok

--- canvas.js
+++ canvas.js
@@ -1,18 +1,53 @@
 const canvas = document.getElementById("canvas")
 canvas.width = document.documentElement.clientWidth
-canvas.height = document.documentElement.clientHeight
+canvas.height = document.documentElement.clientHeight -20
 let ctx = canvas.getContext('2d')
 let painting = false
 let prePosition = []
 let paintingColor = 'black'
-
+let divColor = ['#ff0000', '#00ff00', '#000000', '#0000ff', '#ffffff']
 ctx.fillStyle = "paintingColor"
 ctx.strokeStyle = "paintingColor"
-ctx.lineWidth = '10'
+ctx.lineWidth = '4'
 ctx.lineCap = "round"
 
 const divs = document.querySelectorAll('div')
-for()
+for(let i=0 ; i<divs.length ; i++) {
+	divs[i].style.backgroundColor = divColor[i]
+	divs[i].onclick = function(){
+		paintingColor = divColor[i]
+		ctx.fillStyle = divColor[i]
+		ctx.strokeStyle = divColor[i]
+		if(i<divs.length - 1){
+			ctx.lineWidth = '4'
+		}else{
+			ctx.lineWidth = '40'
+		}
+	}
+}
+// divs[i].onclick = function(){
+// 	paintingColor = divColior[i]
+// 	console.log(divColor[i])
+// 	ctx.fillStyle = divColor[i]
+// 	ctx.strokeStyle = divColor[i]
+// }
+
+var isTouchDevice = 'ontouchstart' in document.documentElement
+
+if(isTouchDevice){
+	canvas.ontouchstart = e =>{
+		let x = e.touches[0].clientX
+		let y = e.touches[0].clientY
+		prePosition = [x, y]
+	}
+	canvas.ontouchmove = e => {
+		let x = e.touches[0].clientX
+		let y = e.touches[0].clientY
+		ctx.beginPath()
+		drawLine(prePosition[0], prePosition[1], x, y)
+		prePosition = [x, y]
+	}
+}
 
 canvas.onmousedown = (e) => {
 		painting = true
